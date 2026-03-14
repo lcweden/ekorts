f36791507842267af24e0e15a01d6c105006e763
eden <198768181+lcweden@users.noreply.github.com>
2026-03-14 11:08:03 +0000

    feat: add initial project files and configuration

---
+++ Ekorts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ekorts
+//
+//  Created by Eden Leung on 14/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ekorts/EkortsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EkortsApp.swift
+//  Ekorts
+//
+//  Created by Eden Leung on 14/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct EkortsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
